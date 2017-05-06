798df5ffd69765704ff78b7f4f2038b6ab427a7f
ishu <ishu01735singla@gmail.com>
2017-05-06 16:06:33 +0530

    some changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+
+	def index
+		@posts = Post.all.order('created_at DESC')
+	end
+
+	def show
+		@post = Post.find(params[:id])
+		@posts = Post.all.order('created_at DESC').limit(4).offset(1)
+	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506090737) do
+ActiveRecord::Schema.define(version: 20170506095833) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -46,8 +46,12 @@ ActiveRecord::Schema.define(version: 20170506090737) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
